05fa1bee9b2492a1856f3f28353d30a0478a32f7
shinyahirai <908.shinya@gmail.com>
2015-11-26 14:29:05 +0800

    first commit

---
+++ CoreGraphicsTutorial/PushButtonView.swift
@@ -0,0 +1,21 @@
+//
+//  PushButtonView.swift
+//  CoreGraphicsTutorial
+//
+//  Created by Shinya Hirai on 2015/11/26.
+//  Copyright (c) 2015年 AkiraMaeda. All rights reserved.
+//
+
+import UIKit
+
+class PushButtonView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
